aa3bdab88ae4b7ae758917f77b1089f092065194
Vinicius Ramos <vinicisiusribramos@gmail.com>
2026-03-28 18:16:50 -0300

    fix: adicionar underscore em parâmetros não usados para evitar erro TS6133

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(_req: VercelRequest, res: VercelResponse) {
   res.setHeader('Content-Type', 'application/json');
   
   return res.status(200).json({
